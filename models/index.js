dd3053be715af2a176c3b404c33088a505411c48
Abdi Setiawan <bedikadiryt@gmail.com>
2024-07-21 20:33:55 +0700

    update

--- models/index.js
+++ models/index.js
@@ -2,7 +2,7 @@ const Sequelize = require("sequelize");
 require("dotenv").config();
 
 const sequelize = new Sequelize(
-  `mysql://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`,
+  `mysql://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}:3306/${process.env.DB_NAME}`,
   {
     dialect: "mysql",
   }
