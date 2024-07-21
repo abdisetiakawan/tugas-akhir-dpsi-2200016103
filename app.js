f415a82cc7a8d06766ff5d39addc236615463c83
Abdi Setiawan <bedikadiryt@gmail.com>
2024-07-21 19:53:05 +0700

    mengubah server ke app.js

--- app.js
+++ app.js
@@ -41,6 +41,10 @@ sequelize
     console.error("Error synchronizing database:", err);
   });
 
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
