de691360d9ad96388f358eb506ff064972cbec6e
Abdi Setiawan <bedikadiryt@gmail.com>
2024-07-21 21:32:12 +0700

    update

--- app.js
+++ app.js
@@ -44,6 +44,11 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
+app.get("/home", (req, res) => {
+  res.send("Hello, world!");
+});
+
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
